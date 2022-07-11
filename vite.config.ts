467ec3444145512fed6975ef194fa5826d164a35
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2022-07-11 21:27:58 +0300

    fix: fixes deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import {resolve} from "path";
+import {defineConfig} from "vite";
 import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/memory-game/',
   plugins: [react(), WindiCSS()],
   resolve: {
     alias: {
